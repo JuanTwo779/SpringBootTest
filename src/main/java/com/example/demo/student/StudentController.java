5d3459461301b6ffbd0577bbb74f02440cb76fcf
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-11-03 22:02:32 +1100

    dependency injection

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,27 +1,25 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v2/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired //dependency injection (gets rid of need to know how to construct object)
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Juan",
-                        "juanmangubat@gmail.com",
-                        LocalDate.of(2000, Month.AUGUST, 11),
-                        22
-                )
-        );
+        return studentService.getStudents();
     }
 }
