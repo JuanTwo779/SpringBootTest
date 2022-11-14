40022af57d8173ca8999d5c993a8d4d5a426f146
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-11-14 19:08:36 +1100

    Business code for adding a new student to the DB. Includes validation.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,18 +6,31 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
-@Service //service layer
+@Service //service provider class - spring bean
 public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    @Autowired
+    @Autowired //injects field to constructor instead of having to create new instance
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.
+                findStudentByEmail(student.getEmail());
+
+        //verify if student email is already in DB if not, save it
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email already registered");
+        }else {
+            studentRepository.save(student);
+        }
     }
 }
